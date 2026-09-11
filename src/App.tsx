32c7a5475e55690f258c4efa5c5051a049e925b4
ZiaUddin76 <zia717571@gmail.com>
2026-09-11 15:26:44 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+
+    <h2 className="text-5xl text-green-500">development stack project</h2>
+     
+
+      
+    </>
+  )
+}
+
+export default App
